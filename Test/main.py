64af68989a9006277a7129cb6efc0eaaf9ae08a0
hans d <153737960+hdmGOAT@users.noreply.github.com>
2024-01-18 17:43:54 +0800

    Main additions.
    Added function for profile edit page, No database functionality

--- Test/main.py
+++ Test/main.py
@@ -6,29 +6,27 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from pages.login_page import Ui_MainWindow as LoginPage
 from pages.dashboard2 import Ui_Dashboard_Window as DashboardPage
 
+
 #test
 class MainWindow(QMainWindow):
     def __init__(self):
-        self.reset_password()  # remove this later
-        exit()                  # remove this later
         super(MainWindow, self).__init__()
         self.__seed = "USTEPALCO"
-        self.__db_file = 'USTEPALCO.db'
+        self.__db_file = self.__seed+'.db'
         self.__conn = sqlite3.connect(self.__db_file)
         self.__sql = self.__conn.cursor()
         self.__init_db()
         self.page_view('login')
+        
+    
 
     def __init_db(self):
         self.__sql.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, password TEXT NOT NULL)")
         self.__conn.commit()
 
-    def __check_login(self):
-        email = self.ui.edit_email.text()
-        text_passw = self.ui.edit_password.text()
-        hash_passw = self.secure_password(text_passw) 
-
-        self.__sql.execute("SELECT * FROM users WHERE email = ? AND password = ?;",(email, hash_passw))
+    def __check_login(self, email, password):
+        password = self.secure_password(password) 
+        self.__sql.execute("SELECT * FROM users WHERE email = ? AND password = ?;",(email, password))
         results = self.__sql.fetchall()
         return True if len(results) > 0 else False
     
@@ -50,8 +48,8 @@ class MainWindow(QMainWindow):
         OTP_Characters = list('ABCDEFGHJKLOPQRX132907')
         OTP_code = random.choices(OTP_Characters, k=OTP_Length)
         return ''.join(OTP_code)
-    
-    def reset_password(self):
+
+    def reset_password(self, email='abales.anikinluk3@gmail.com'):
         reset_code = self.generate_otp()
         user = "admin" # temp variable example  for later use
         subject = "Password reset on USTEPALCO"
@@ -78,7 +76,7 @@ class MainWindow(QMainWindow):
         </div>
         """
         mail = Mail(subject, content, "html")
-        mail.sendto("abales.anikinluk3@gmail.com")
+        mail.sendto(email)
 
     def page_view(self, view):
         if view == 'login':
@@ -88,13 +86,58 @@ class MainWindow(QMainWindow):
 
         self.ui.setupUi(self)
 
+    def on_logout_button_pressed(self):
+        response = QMessageBox.question(self, 'Logout', 'Are you sure you want to logout?',
+        QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if response == QMessageBox.Yes:
+            self.page_view('login')
+
+
     def on_button_login_pressed(self):
-        if self.__check_login():
+      '''  email = self.ui.edit_email.text()
+        passw = self.ui.edit_password.text()
+        if((len(passw) < 8)):
+            self.show_message("ERROR", "Minimum password length is 8.", QMessageBox.Warning)
+        elif(self.__check_login(email, passw) ):
             self.page_view('dashboard')
         else:
-            self.show_message("ERROR", "Incorrect login credentials!", QMessageBox.Warning)
+            self.show_message("ERROR", "Incorrect login credentials!", QMessageBox.Warning)'''
+      self.page_view('dashboard')
 
-if __name__ == "__main__":
+
+    # irename pod guro ning mga button rald, unsa man ning "pushButton_2"
+    def on_pushButton_2_clicked(self):
+        global p_kwh 
+        self.p_kwh = 0.6232
+        self.amountDue = self.p_kwh * 99 #placeholder
+        self.ui.billDue_edit.setText("₱ " + str(format(self.amountDue, "3.3f")))
+
+    
+
+    def on_profileEdit_button_pressed(self):
+        
+        if not self.ui.profileEdit_button.isChecked(): #Checks if button has already been clicked to edit
+            self.ui.adminID_edit.setReadOnly(False)
+            self.ui.name_edit.setReadOnly(False)
+            self.ui.adrress_edit.setReadOnly(False)
+            self.ui.contactNo_edit.setReadOnly(False)
+            self.ui.email_edit.setReadOnly(False)
+            self.ui.password_edit.setReadOnly(False)
+            self.ui.profileEdit_button.setText("Done")
+
+        else:                              #if button has already been clicked to edit it puts it all back in read only
+            self.ui.adminID_edit.setReadOnly(True)
+            self.ui.name_edit.setReadOnly(True)
+            self.ui.adrress_edit.setReadOnly(True)
+            self.ui.contactNo_edit.setReadOnly(True)
+            self.ui.email_edit.setReadOnly(True)
+            self.ui.password_edit.setReadOnly(True)
+            self.ui.profileEdit_button.setText("Edit")
+
+
+        
+
+if(__name__ == "__main__"):
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
