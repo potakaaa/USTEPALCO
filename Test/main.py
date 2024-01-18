ba6f3183cd43418cbd5afc05644e75a597c3fe4d
hans d <153737960+hdmGOAT@users.noreply.github.com>
2024-01-19 00:12:43 +0800

    merged shit

--- Test/main.py
+++ Test/main.py
@@ -6,23 +6,19 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from pages.login_page import Ui_MainWindow as LoginPage
 from pages.dashboard2 import Ui_Dashboard_Window as DashboardPage
 
-
+#test
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.__seed = "USTEPALCO"
-        self.__db_file = self.__seed+'.db'
+        self.__db_file = "USTEPALCO//Test//USTEPALCO.db"
         self.__conn = sqlite3.connect(self.__db_file)
         self.__sql = self.__conn.cursor()
-        self.__session_user_uid = ""
-        self.__session_username = ""
-        self.__init_db()
         self.page_view('login')
+        global p_kwh 
+        self.p_kwh = 11.3997
 
 
-    def __init_db(self):
-        self.__sql.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, password TEXT NOT NULL)")
-        self.__conn.commit()
 
     def __check_login(self, email, password):
         password = self.secure_password(password) 
@@ -61,8 +57,8 @@ class MainWindow(QMainWindow):
         msg.setText(text)
         msg.exec_()
 
-    def generate_otp(self): # Generate - One Time Password
-        OTP_Length = 8
+    def generate_otp(self):
+        OTP_Length = 6
         OTP_Characters = list('ABCDEFGHJKLOPQRX132907')
         OTP_code = random.choices(OTP_Characters, k=OTP_Length)
         return ''.join(OTP_code)
@@ -100,6 +96,164 @@ class MainWindow(QMainWindow):
         mail.sendto(email)
         self.__update_password(temporary_password, email)
 
+    def on_dashboard_button_pressed(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.dashboard_button.setStyleSheet("#dashboard_button {\n"
+            "color: white; }\n"
+        )
+        self.ui.reports_button.setStyleSheet("#reports_button {\n"
+            "color: #959595; }")
+        self.ui.manage_button.setStyleSheet("#manage_button {\n"
+            "color: #959595; }")
+        self.ui.generate_button.setStyleSheet("#generate_button {\n"
+            "color: #959595; }")
+        self.ui.profile_button.setStyleSheet("#profile_button {\n"
+            "color: #959595; }")
+        
+    def on_reports_button_pressed(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+        self.__users = [self.ui.paymentUsers_row1, self.ui.paymentUsers_row2, 
+                        self.ui.paymentUsers_row3, self.ui.paymentUsers_row4, 
+                        self.ui.paymentUsers_row5, self.ui.paymentUsers_row6, 
+                        self.ui.paymentUsers_row7, self.ui.paymentUsers_row8]
+        self.__ad = [self.ui.paymentAddress_row1, self.ui.paymentAddress_row2, 
+                        self.ui.paymentAddress_row3, self.ui.paymentAddress_row4, 
+                        self.ui.paymentAddress_row5, self.ui.paymentAddress_row6, 
+                        self.ui.paymentAddress_row7, self.ui.paymentAddress_row8]
+        self.__dates = [self.ui.paymentDate_row1, self.ui.paymentDate_row2, 
+                        self.ui.paymentDate_row3, self.ui.paymentDate_row4, 
+                        self.ui.paymentDate_row5, self.ui.paymentDate_row6, 
+                        self.ui.paymentDate_row7, self.ui.paymentAddress_row8]
+        self.__usage = [self.ui.paymentAmount_row1, self.ui.paymentAmount_row2, 
+                        self.ui.paymentAmount_row3, self.ui.paymentAmount_row4, 
+                        self.ui.paymentAmount_row5, self.ui.paymentAmount_row6, 
+                        self.ui.paymentAmount_row7, self.ui.paymentAmount_row8]
+        
+        self.ui.stackedWidget.setCurrentIndex(2)
+        self.ui.reports_button.setStyleSheet("#reports_button {\n"
+            "color: white; }\n"
+        )
+        self.ui.dashboard_button.setStyleSheet("#dashboard_button {\n"
+            "color: #959595; }")
+        self.ui.manage_button.setStyleSheet("#manage_button {\n"
+            "color: #959595; }")
+        self.ui.generate_button.setStyleSheet("#generate_button {\n"
+            "color: #959595; }")
+        self.ui.profile_button.setStyleSheet("#profile_button {\n"
+            "color: #959595; }")
+        
+        self.__sql.execute("SELECT * FROM payment_det ORDER BY date_payment DESC;")
+
+        for i in range(len(self.__users)):
+            
+            self.__sql.execute("SELECT full_name FROM payment_det")
+            namcong = self.__sql.fetchall()
+
+            self.__users[i].setText(str(namcong[i])[2:len(str(namcong[i])) - 3])
+
+            self.__sql.execute("SELECT address FROM payment_det")
+            adcong = self.__sql.fetchall()
+            self.__ad[i].setText(str(adcong[i])[2:len(str(adcong[i])) - 3])
+
+            self.__sql.execute("SELECT date_payment FROM payment_det")
+            datcong = self.__sql.fetchall()
+            self.__dates[i].setText(str(datcong[i])[2:len(str(datcong[i])) - 3])
+
+            self.__sql.execute("SELECT usage FROM payment_det")
+            usagcong = self.__sql.fetchall()
+
+            usag = int(str(usagcong[i])[1:len(str(usagcong[i])) - 2]) * self.p_kwh
+            self.__usage[i].setText("₱ "+ str(format(usag, "3.3f")))  
+
+    def on_manage_button_pressed(self):
+        
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+        self.__names = [self.ui.row1_name, self.ui.row1_name_2, self.ui.row1_name_3, self.ui.row2_name, 
+                    self.ui.row2_name_2, self.ui.row2_name_3, self.ui.row3_name, self.ui.row3_name_2, 
+                    self.ui.row3_name_3, self.ui.row4_name, self.ui.row4_name_2, self.ui.row4_name_3,
+                    self.ui.row5_name, self.ui.row5_name_2, self.ui.row5_name_3, self.ui.row6_name, 
+                    self.ui.row6_name_2, self.ui.row6_name_3]
+            
+        self.__address = [self.ui.row1_address, self.ui.row1_address_2, self.ui.row1_address_3, self.ui.row2_address, 
+                    self.ui.row2_address_2, self.ui.row2_address_3, self.ui.row3_address, self.ui.row3_address_2, 
+                    self.ui.row3_address_3, self.ui.row4_address, self.ui.row4_address_2, self.ui.row4_address_3,
+                    self.ui.row5_address, self.ui.row5_address_2, self.ui.row5_address_3, self.ui.row6_address, 
+                    self.ui.row6_address_2, self.ui.row6_address_3]
+            
+        self.__contractNo = [self.ui.row1_contractNo, self.ui.row1_contractNo_2, self.ui.row1_contractNo_3, self.ui.row2_contractNo, 
+                    self.ui.row2_contractNo_2, self.ui.row2_contractNo_3, self.ui.row3_contractNo, self.ui.row3_contractNo_2, 
+                    self.ui.row3_contractNo_3, self.ui.row4_contractNo, self.ui.row4_contractNo_2, self.ui.row4_contractNo_3,
+                    self.ui.row5_contractNo, self.ui.row5_contractNo_2, self.ui.row5_contractNo_3, self.ui.row6_contractNo, 
+                    self.ui.row6_contractNo_2, self.ui.row6_contractNo_3]
+            
+        self.__amount = [self.ui.row1_amount, self.ui.row1_amount_2, self.ui.row1_amount_3, self.ui.row2_amount, 
+                    self.ui.row2_amount_2, self.ui.row2_amount_3, self.ui.row3_amount, self.ui.row3_amount_2, 
+                    self.ui.row3_amount_3, self.ui.row4_amount, self.ui.row4_amount_2, self.ui.row4_amount_3,
+                    self.ui.row5_amount, self.ui.row5_amount_2, self.ui.row5_amount_3, self.ui.row6_amount, 
+                    self.ui.row6_amount_2, self.ui.row6_amount_3]
+        
+        self.ui.users_StackedWidget.setCurrentIndex(0)
+
+        for i in range(len(self.__names)):
+            
+            self.__sql.execute("SELECT full_name FROM users")
+            namcon = self.__sql.fetchall()
+            self.__names[i].setText(str(namcon[i])[2:len(str(namcon[i])) - 3])
+
+            self.__sql.execute("SELECT address FROM users")
+            adcon = self.__sql.fetchall()
+            self.__address[i].setText(str(adcon[i])[2:len(str(adcon[i])) - 3])
+
+            self.__sql.execute("SELECT contract_No FROM users")
+            concon = self.__sql.fetchall()
+            self.__contractNo[i].setText(str(concon[i])[2:len(str(concon[i])) - 3])
+
+            self.__sql.execute("SELECT amount_due FROM users")
+            amcon = self.__sql.fetchall()
+            self.__amount[i].setText("₱ " + str(amcon[i])[1:len(str(amcon[i])) - 2])
+
+        self.ui.manage_button.setStyleSheet("#manage_button {\n"
+            "color: white; }\n"
+        )
+        self.ui.reports_button.setStyleSheet("#reports_button {\n"
+            "color: #959595; }")
+        self.ui.dashboard_button.setStyleSheet("#dashboard_button {\n"
+            "color: #959595; }")
+        self.ui.generate_button.setStyleSheet("#generate_button {\n"
+            "color: #959595; }")
+        self.ui.profile_button.setStyleSheet("#profile_button {\n"
+            "color: #959595; }")
+    def on_generate_button_pressed(self):
+        self.ui.stackedWidget.setCurrentIndex(4)
+        self.ui.generate_button.setStyleSheet("#generate_button {\n"
+            "color: white; }\n"
+        )
+        self.ui.reports_button.setStyleSheet("#reports_button {\n"
+            "color: #959595; }")
+        self.ui.manage_button.setStyleSheet("#manage_button {\n"
+            "color: #959595; }")
+        self.ui.dashboard_button.setStyleSheet("#dashboard_button {\n"
+            "color: #959595; }")
+        self.ui.profile_button.setStyleSheet("#profile_button {\n"
+            "color: #959595; }")
+        
+    def on_profile_button_pressed(self):
+        self.ui.stackedWidget.setCurrentIndex(5) 
+        self.ui.profile_button.setStyleSheet("#profile_button {\n"
+            "color: white; }\n"
+        )
+        self.ui.reports_button.setStyleSheet("#reports_button {\n"
+            "color: #959595; }")
+        self.ui.manage_button.setStyleSheet("#manage_button {\n"
+            "color: #959595; }")
+        self.ui.generate_button.setStyleSheet("#generate_button {\n"
+            "color: #959595; }")
+        self.ui.dashboard_button.setStyleSheet("#dashboard_button {\n"
+            "color: #959595; }")   
+
+
     def page_view(self, view):
         if view == 'login':
             self.ui = LoginPage()
@@ -114,17 +268,19 @@ class MainWindow(QMainWindow):
         if response == QMessageBox.Yes:
             self.page_view('login')
 
+
     def on_button_login_pressed(self):
-        email = self.ui.edit_email.text()
+        '''email = self.ui.edit_email.text()
         passw = self.ui.edit_password.text()
-        if((len(passw) < 0)):
+        if((len(passw) < 8)):
             self.show_message("ERROR", "Minimum password length is 8.", QMessageBox.Warning)
         elif(self.__check_login(email, passw) ):
             self.page_view('dashboard')
         else:
-            self.show_message("ERROR", "Incorrect login credentials!", QMessageBox.Warning)
-    #   self.page_view('dashboard')
+            self.show_message("ERROR", "Incorrect login credentials!", QMessageBox.Warning)'''
 
+        self.page_view('dashboard')
+        
     def on_forget_button_pressed(self):
         email = self.ui.edit_email.text()
         if(email.strip() == ''):
@@ -132,16 +288,17 @@ class MainWindow(QMainWindow):
             return
         self.reset_password(email)
 
-
     # irename pod guro ning mga button rald, unsa man ning "pushButton_2"
     def on_pushButton_2_clicked(self):
-        global p_kwh 
-        self.p_kwh = 0.6232
+
         self.amountDue = self.p_kwh * 99 #placeholder
         self.ui.billDue_edit.setText("₱ " + str(format(self.amountDue, "3.3f")))
 
 
+        
+
     def on_profileEdit_button_pressed(self):
+        self.ui.profileEdit_button.setCheckable(True)
 
         if not self.ui.profileEdit_button.isChecked(): #Checks if button has already been clicked to edit
             self.ui.adminID_edit.setReadOnly(False)
@@ -161,9 +318,18 @@ class MainWindow(QMainWindow):
             self.ui.password_edit.setReadOnly(True)
             self.ui.profileEdit_button.setText("Edit")
 
+    def on_next_button_pressed(self):     
+        nextwidg = self.ui.users_StackedWidget.currentIndex() + 1
 
+        if nextwidg < 3:
+            self.ui.users_StackedWidget.setCurrentIndex(nextwidg)
+        else:
+            self.ui.users_StackedWidget.setCurrentIndex(0)
+        
         
 
+        
+        
 if(__name__ == "__main__"):
     app = QApplication(sys.argv)
     window = MainWindow()
